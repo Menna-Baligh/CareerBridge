0c90525fc21ab92a94ccb9f7a1df7311eb1c6fdc
Menna-Baligh <mennabaligh55520@yahoo.com>
2025-09-29 22:41:26 +0300

    Add initial UI layout for the 3 main widgets using static data for styling

--- Job-Backoffice/resources/views/dashboard/index.blade.php
+++ Job-Backoffice/resources/views/dashboard/index.blade.php
@@ -5,12 +5,25 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+    <div class="py-12 px-6">
+        {{-- overview cards --}}
+        <div class="grid grid-cols-3 gap-6">
+            <div class="p-6 bg-white overflow-hidden shadow-sm rounded-lg">
+                <h3 class="text-lg font-medium text-gray-600">Active Users</h3>
+                <p class="text-3xl font-bold text-indigo-600">100</p>
+                <p class="text-sm text-gray-600">Last 30 Days</p>
+            </div>
+
+            <div class="p-6 bg-white overflow-hidden shadow-sm rounded-lg">
+                <h3 class="text-lg font-medium text-gray-600">Total Jobs</h3>
+                <p class="text-3xl font-bold text-indigo-600">100</p>
+                <p class="text-sm text-gray-600">All Time</p>
+            </div>
+
+            <div class="p-6 bg-white overflow-hidden shadow-sm rounded-lg">
+                <h3 class="text-lg font-medium text-gray-600">Total Applications</h3>
+                <p class="text-3xl font-bold text-indigo-600">100</p>
+                <p class="text-sm text-gray-600">All Time</p>
             </div>
         </div>
     </div>
